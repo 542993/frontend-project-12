aebd024f78abcc90dbde4406b77a75561a22e5b9
Alexandr <ustus777@mail.ru>
2023-09-12 21:14:49 +0300

    update notFoundPage

--- frontend/src/Components/Pages/NotFoundPage.jsx
+++ frontend/src/Components/Pages/NotFoundPage.jsx
@@ -1,16 +1,22 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import Header from '../Header';
+import img from '../../assets/notFound.svg';
+import { routesApp } from '../../routes';
 
-function NotFoundPage() {
+const NotFoundPage = () => {
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-
-      <div>
-        You may comback to main page <Link to="/">ССылка</Link>
+    <div className="d-flex flex-column h-100">
+      <Header />
+      <div className="text-center">
+        <img alt="Страница не найдена" src={img} className="img-fluid h-25" />
+        <h1 className="h4 text-muted">Страница не найдена</h1>
+        <p className="text-muted">
+        Но вы можете перейти
+          <Link to={routesApp.homePage}>на главную страницу</Link>
+        </p>
       </div>
     </div>
   );
-}
+};
 
 export default NotFoundPage;
