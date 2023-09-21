4758bff17dd46b16817bd64c777b70932abccaf9
Alexandr <ustus777@mail.ru>
2023-09-21 22:41:29 +0300

    add dropdown button

--- frontend/src/Components/ChannelsList.js
+++ frontend/src/Components/ChannelsList.js
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { Button } from 'react-bootstrap';
+import { Button, Dropdown, ButtonGroup } from 'react-bootstrap';
 import { setCurrentChannel, selectors } from '../slices/channelsSlice';
+import { setActiveModal } from '../slices/modalSlice';
 
 const ChannelsList = () => {
   const channels = useSelector(selectors.selectAll);
@@ -9,9 +10,18 @@ const ChannelsList = () => {
 
   const channelsList = channels.map((channel) => {
     const variant = channel.id === currentChannelId ? 'secondary' : null;
-
+    const dropDownButton = (
+      <>
+        <Dropdown.Toggle split variant={variant} id="dropdown-split-basic" />
+        <Dropdown.Menu>
+          <Dropdown.Item onClick={() => dispatch(setActiveModal({ type: 'delete' }))}>Удалить</Dropdown.Item>
+          <Dropdown.Item onClick={() => dispatch(setActiveModal({ type: 'rename' }))}>Переименовать</Dropdown.Item>
+        </Dropdown.Menu>
+      </>
+    );
     return (
       <li key={channel.id} className="nav-item w-100">
+        <Dropdown as={ButtonGroup}>
           <Button
             className="w-100 rounded-0 text-start text-truncate border-0"
             onClick={() => dispatch(setCurrentChannel(channel.id))}
@@ -19,6 +29,8 @@ const ChannelsList = () => {
           >
             {`# ${channel.name}`}
           </Button>
+        {channel.removable && dropDownButton}
+        </Dropdown>
       </li>
     );
   });
