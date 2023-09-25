d77c4d51f8b71b1714c93384d72135144efb81d9
Alexandr <ustus777@mail.ru>
2023-09-25 20:53:41 +0300

    rollbarr

--- frontend/src/Components/RollbarProvider.js
+++ frontend/src/Components/RollbarProvider.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Provider, ErrorBoundary } from '@rollbar/react';
 
 const rollbarConfig = {
-  accessToken: '3ed12951194c4bfd81d9cd11052481cf',
-  environment: 'testenv',
+  accessToken: 'POST_CLIENT_ITEM_ACCESS_TOKEN',
+  environment: 'production',
 };
 const RollbarProvider = ({ children }) => (
   <Provider config={rollbarConfig}>
